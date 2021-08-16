ed4b70a7d3655136cd6fc64743099096a162cefb
Ulf Hermjakob <ulf@D22ML-ULF.local>
2021-08-16 16:50:36 -0700

    minor reformat; added comment

--- aux/tok-boost.py
+++ aux/tok-boost.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-"""This script re-tokenizes a few things, so that there is less clutter (fewer differences)
-in comparison with utoken."""
+"""This script re-tokenizes a few things, so that there is less clutter (fewer differences) in comparison
+with utoken. Usage: tok-boost.py < STDIN > STDOUT. Suitable for --b1/b2 in colot-mt-diff.pl"""
 
 import re
 import sys
@@ -14,7 +14,8 @@ if __name__ == "__main__":
        line = re.sub(r" (['’]) (['’]) ", r" \1\2 ", line)
        line = re.sub(r" (['’]) (['’])$", r" \1\2", line)
        line = re.sub(r" (['’]) (d|ll|m|re|s|ve)\b", r" \1\2", line, flags=re.IGNORECASE)
-       line = re.sub(r"\b(are|could|did|do|does|had|has|have|is|shall|should|was|were|will|would)n (['’])t\b", r"\1 n\2t", line, flags=re.IGNORECASE)
+       line = re.sub(r"\b(are|could|did|do|does|had|has|have|is|shall|should|was|were|will|would)n (['’])t\b",
+                     r"\1 n\2t", line, flags=re.IGNORECASE)
        line = re.sub(r"\bca n(['’])t\b", r"can n\1t", line, flags=re.IGNORECASE)
        line = re.sub(r"\bcan (['’])t\b", r"can n\1t", line, flags=re.IGNORECASE)
        line = re.sub(r"\bwo n(['’])t\b", r"will n\1t", line, flags=re.IGNORECASE)
