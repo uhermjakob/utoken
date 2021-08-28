6e19a657c7213ac258ea72576884c0228e099dd8
Ulf Hermjakob <ulf@D22ML-ULF.local>
2021-08-27 22:31:39 -0700

    First version

---
+++ aux/detok-boost.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""This script de-tokenizes a few things, so that there is less clutter (fewer differences) in comparison
+with detokenize. Usage: detok-boost.py < STDIN > STDOUT. Suitable for --b1/b2 in colot-mt-diff.pl"""
+
+import re
+import regex
+import sys
+
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line = ' ' + line.strip() + ' '
+        line = re.sub('', '™', line)
+        line = re.sub('', '‘', line)
+        line = re.sub('', '’', line)
+        line = re.sub('', '”', line)
+        line = re.sub('', '“', line)
+        line = re.sub('', '–', line)
+        line = re.sub('', '—', line)
+        for _ in range(2):
+            line = re.sub(r" (['’])(d|em|m|re|s|ve) ", r'\1\2 ', line)
+            line = re.sub(r'(\d) ([%]) ', r'\1\2 ', line)
+            line = re.sub(r' ([$]|US$|RMB) (\d)', r' \1\2', line)
+            line = regex.sub(r'(\pL) ([:]) ', r'\1\2 ', line)
+            line = re.sub(r' ([(]) ', r' \1', line)
+            line = re.sub(r' ([.,;)]|!+|\?+) ', r'\1 ', line)
+            line = re.sub(r' (can) (not) ', r' \1\2 ', line, flags=re.IGNORECASE)
+            line = re.sub(r"\b(are|ca|could|did|do|does|had|has|have|is|sha|should|was|were|wo|would) ?n['’]?t\b",
+                          r"\1n't", line, flags=re.IGNORECASE)
+        print(line.strip())
+
