e4718e86eee5635d434be2f2474240fe20e9af84
Ulf Hermjakob <ulf@D22ML-ULF.local>
2021-09-06 18:00:20 -0700

    added MT-tok tag removal

--- aux/boost-tok.py
+++ aux/boost-tok.py
@@ -8,21 +8,23 @@ import sys
 
 if __name__ == "__main__":
     for line in sys.stdin:
-       for _ in range(2):
-           line = re.sub('! !', '!!', line)
-           line = re.sub('\? \?', '??', line)
-           line = re.sub('‹ ‹', '‹‹', line)
-           line = re.sub('› ›', '››', line)
-       line = re.sub(r" (['’]) (['’]) ", r" \1\2 ", line)
-       line = re.sub(r" (['’]) (['’])$", r" \1\2", line)
-       line = re.sub(r" (['’]) (d|ll|m|re|s|ve)\b", r" \1\2", line, flags=re.IGNORECASE)
-       line = re.sub(r"\b(are|could|did|do|does|had|has|have|is|shall|should|was|were|will|would)n (['’])t\b",
-                     r"\1 n\2t", line, flags=re.IGNORECASE)
-       line = re.sub(r"\bca n(['’])t\b", r"can n\1t", line, flags=re.IGNORECASE)
-       line = re.sub(r"\bcan (['’])t\b", r"can n\1t", line, flags=re.IGNORECASE)
-       line = re.sub(r"\bwo n(['’])t\b", r"will n\1t", line, flags=re.IGNORECASE)
-       line = re.sub(r"\bwon (['’])t\b", r"will n\1t", line, flags=re.IGNORECASE)
-       line = re.sub(r"\b(can)(not)\b", r"\1 \2", line, flags=re.IGNORECASE)
-       line = re.sub(r" @-@ (in) @-@ (law)\b", r"-\1-\2", line, flags=re.IGNORECASE)
-       print(line.rstrip())
-
+        line = ' ' + line.strip() + ' '
+        for _ in range(2):
+            line = re.sub('! !', '!!', line)
+            line = re.sub('\? \?', '??', line)
+            line = re.sub('‹ ‹', '‹‹', line)
+            line = re.sub('› ›', '››', line)
+            line = re.sub(r" @((?:'+|:|’+|‘’|-+|\*|_|”|“|\"+|~|/|–+|‘+|\.\.+|\++|—+|`|&quot;)@?) ", r" \1 ", line)
+            line = re.sub(r" ('+|:|’+|‘’|-+|\*|_|”|“|\"+|~|/|–+|‘+|\.\.+|\++|—+|`|&quot;)@ ", r" \1 ", line)
+        line = re.sub(r" (['’]) (['’]) ", r" \1\2 ", line)
+        line = re.sub(r" (['’]) (['’])$", r" \1\2", line)
+        line = re.sub(r" (['’]) (d|ll|m|re|s|ve)\b", r" \1\2", line, flags=re.IGNORECASE)
+        line = re.sub(r"\b(are|could|did|do|does|had|has|have|is|shall|should|was|were|will|would)n (['’])t\b",
+                      r"\1 n\2t", line, flags=re.IGNORECASE)
+        line = re.sub(r"\bca n(['’])t\b", r"can n\1t", line, flags=re.IGNORECASE)
+        line = re.sub(r"\bcan (['’])t\b", r"can n\1t", line, flags=re.IGNORECASE)
+        line = re.sub(r"\bwo n(['’])t\b", r"will n\1t", line, flags=re.IGNORECASE)
+        line = re.sub(r"\bwon (['’])t\b", r"will n\1t", line, flags=re.IGNORECASE)
+        line = re.sub(r"\b(can)(not)\b", r"\1 \2", line, flags=re.IGNORECASE)
+        line = re.sub(r" @-@ (in) @-@ (law)\b", r"-\1-\2", line, flags=re.IGNORECASE)
+        print(line.strip())
