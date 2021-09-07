9a583f131413f889c8c8c53c4d75bffc4223ef96
Ulf Hermjakob <ulf@D22ML-ULF.local>
2021-09-06 18:01:06 -0700

    added booster files

--- aux/tok-test.py
+++ aux/tok-test.py
@@ -110,7 +110,7 @@ if __name__ == "__main__":
                 ref_legend_s = None
                 if lang_code and lang_code != 'eng':
                     english_filename = re.sub(r'\.[a-z]{3}\.txt$', '.eng.txt', input_filename)
-                    sys.stderr.write(f"**** {input_filename} to {english_filename}\n")
+                    # sys.stderr.write(f"**** {input_filename} to {english_filename}\n")
                     if os.path.isfile(english_filename):
                         ref_file_s = f' {input_filename} {english_filename}'
                         ref_legend_s = f' {lang_code}.txt eng.txt'
@@ -119,19 +119,28 @@ if __name__ == "__main__":
                     ref_legend_s = f' {lang_code}.txt'
                 sacremoses_viz_filename = os.path.join(public_test_data_dir, 'viz',
                                                        f'{core_filename}.sacrem-utoken-diff.html')
+                sys.stderr.write(f"boost ...\n")
+                b1_command = f'boost-tok.py < {output_filename} > {output_filename}.boost'
+                b2_command = f'boost-tok.py < {old_ulf_tokenizer_filename} > {old_ulf_tokenizer_filename}.boost'
+                b3_command = f'boost-tok.py < {sacremoses_filename} > {sacremoses_filename}.boost'
+                subprocess.run(b1_command, shell=True)
+                subprocess.run(b2_command, shell=True)
+                subprocess.run(b3_command, shell=True)
                 command = f'color-mt-diffs.pl {sacremoses_filename} {output_filename}{ref_file_s}' \
+                          f' -b {sacremoses_filename}.boost {output_filename}.boost' \
                           f' -l sacrem utoken{ref_legend_s}' \
                           f' -o {sacremoses_viz_filename}'
-                sys.stderr.write(f"{command} ...\n")
+                # sys.stderr.write(f"{command} ...\n")
                 sys.stderr.write(f"color-mt-diffs.pl sacremoses {input_filename}"
                                  f" -out {sacremoses_viz_filename} ...\n")
                 subprocess.run(command, shell=True)
                 old_ulf_tokenizer_viz_filename = os.path.join(public_test_data_dir, 'viz',
                                                               f'{core_filename}.old-u-t-utoken-diff.html')
                 command = f'color-mt-diffs.pl {old_ulf_tokenizer_filename} {output_filename}{ref_file_s}' \
+                          f' -b {old_ulf_tokenizer_filename}.boost {output_filename}.boost' \
                           f' -l old-u-t utoken{ref_legend_s}' \
                           f' -o {old_ulf_tokenizer_viz_filename}'
-                sys.stderr.write(f"{command} ...\n")
+                # sys.stderr.write(f"{command} ...\n")
                 sys.stderr.write(f"color-mt-diffs.pl old ulf-tokenizer {input_filename}"
                                  f" -o {old_ulf_tokenizer_viz_filename} ...\n")
                 subprocess.run(command, shell=True)
