672124256fbeca6d52057a68df089b7708681a5d
Ulf Hermjakob <ulf@D22ML-ULF.local>
2021-09-13 18:14:02 -0700

    Added an input line

--- utoken/sample-use.py
+++ utoken/sample-use.py
@@ -4,4 +4,5 @@ import utokenize
 
 tok = utokenize.Tokenizer(lang_code='eng')  # Initialize tokenizer, load resources
 print(tok.tokenize_string("Dont worry!"))
+print(tok.tokenize_string("Sold,for $9,999.99 on ebay.com."))
 
