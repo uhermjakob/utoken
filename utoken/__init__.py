c407cd5ed124df132b5765c5f243f07a8d682901
Ulf Hermjakob <ulf@isi.edu>
2021-09-15 16:49:05 -0700

    new date

--- utoken/__init__.py
+++ utoken/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 __description__ = '''utoken is a tokenizer that divides text into words, punctuation and special tokens such as
  numbers, URLs, XML tags, email-addresses and hashtags. It comes with a companion detokenizer.'''
-last_mod_date = 'September 14, 2021'
+last_mod_date = 'September 15, 2021'
