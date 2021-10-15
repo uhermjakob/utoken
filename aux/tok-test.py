8fd17db265c2d77f01d0da4a74e842dee38ba046
Ulf Hermjakob <ulf@isi.edu>
2021-10-15 15:53:35 -0700

    +Urdu

--- aux/tok-test.py
+++ aux/tok-test.py
@@ -92,6 +92,7 @@ if __name__ == "__main__":
                                'tel.txt',
                                'tgl.txt',
                                'tur.txt',
+                               'urd.txt',
                                'vie.txt',
                                'xho.txt',
                                'yor.txt',
