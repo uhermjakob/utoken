3ccd79965b0f33238e8b600c16cd289bba9aeae7
Ulf Hermjakob <ulf@isi.edu>
2021-10-18 20:13:38 -0700

    +Danish

--- aux/tok-test.py
+++ aux/tok-test.py
@@ -63,6 +63,7 @@ if __name__ == "__main__":
                                'cat.txt',
                                'ces.txt',
                                'cym.txt',
+                               'dan.txt',
                                'deu.txt',
                                'ell.txt',
                                'eng.txt',
